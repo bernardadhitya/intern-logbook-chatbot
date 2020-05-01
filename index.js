e254b136256a64bc135020582ee230d82d03d2c4
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-05-01 21:07:57 +0700

    [Bernard] Implement form filler in index.js with puppeteer

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
+const puppeteer = require('puppeteer');
 
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -21,13 +22,64 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     });
 });
 
+async function fill(desc){
+    const data = {
+        username: process.env.MY_USERNAME,
+        password: process.env.MY_PASSWORD,
+        clock: {
+            in: "09.00",
+            out: "17.00"
+        },
+        activity: "",
+        description: desc
+    }
+    
+    let url = {
+        loginPage: 'https://industry.socs.binus.ac.id/learning-plan/auth/login',
+        logBookPage: 'https://industry.socs.binus.ac.id/learning-plan/student/log-book'
+    }
+
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+
+    await page.goto(url.loginPage, {waitUntil: 'networkidle2'});
+
+    //fill login
+    await page.evaluate(function(data){
+        document.querySelector('input[name="username"]').value = data.username;
+        document.querySelector('input[name="password"]').value = data.password;
+        document.forms[0].submit();
+    }, data);
+
+    await page.waitFor(5000);
+    await page.goto(url.logBookPage, {waitUntil: 'networkidle2'});
+
+    //fill logbook
+    await page.evaluate(function(data){
+        //check if its day off
+        let currDate = new Date(Date.now());
+        if(currDate.getDay() == 0 || currDate.getDay() == 6){
+            document.querySelector('input[name="clock-in"]').value = data.clock.in;
+            document.querySelector('input[name="clock-out"]').value = data.clock.out;
+            document.querySelector('input[name="activity"]').value = data.activity;
+            document.querySelector('textarea').value = data.description;
+        }
+        document.forms[4].submit();
+    }, data);
+
+    await page.waitFor(5000);
+    await browser.close();
+}
+
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
     return Promise.resolve(null);
   }
 
-  const echo = { type: 'text', text: event.message.text };
-  return client.replyMessage(event.replyToken, echo);
+  const message = { type: 'text', text: event.message.text };
+  await fill(message.text);
+
+  return client.replyMessage(event.replyToken, "Your message has been posted!");
 }
 
 const port = process.env.PORT || 3000;
