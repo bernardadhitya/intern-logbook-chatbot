7fcdd00ba467c468e195f409eefe6fd0e4dda5aa
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-05-01 11:44:04 +0700

    [Bernard] Add Hello world in index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const config = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET
+};
+
+const client = new line.Client(config);
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendStatus(404);
+});
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+    Promise
+        .all(req.body.events.map(mainProgram))
+        .then((result) => res.json(result))
+        .catch((error) => {
+            console.error(`Promise error ${error}`);
+        });
+});
+
+function mainProgram(event){
+    if(event.type !== message || event.message.type !== 'text'){
+        return Promise.result(null);
+    }
+    return client.replyMessage(event.replyToken, {type: 'text', text: 'Hello world'});
+}
+
+const port = 3000;
+app.listen(port, () => {});
