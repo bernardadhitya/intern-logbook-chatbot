716520e6f4a19b92c4bf8a8cc1e443a720ff7c46
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-05-01 21:25:59 +0700

    [Bernard] Rollback

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 
 const line = require('@line/bot-sdk');
 const express = require('express');
-//const puppeteer = require('puppeteer');
 
+// create LINE SDK config from env variables
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.CHANNEL_SECRET,
 };
 
+// create LINE SDK client
 const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
 const app = express();
 
-app.post('/webhook', line.middleware(config), (req, res) => {
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
@@ -22,62 +28,21 @@ app.post('/webhook', line.middleware(config), (req, res) => {
     });
 });
 
-/*async function fill(desc){
-    const data = {
-        username: process.env.MY_USERNAME,
-        password: process.env.MY_PASSWORD,
-        clock: {
-            in: "09.00",
-            out: "17.00"
-        },
-        activity: "",
-        description: desc
-    }
-    
-    let url = {
-        loginPage: 'https://industry.socs.binus.ac.id/learning-plan/auth/login',
-        logBookPage: 'https://industry.socs.binus.ac.id/learning-plan/student/log-book'
-    }
-
-    let browser = await puppeteer.launch();
-    let page = await browser.newPage();
-
-    await page.goto(url.loginPage, {waitUntil: 'networkidle2'});
-
-    //fill login
-    await page.evaluate(function(data){
-        document.querySelector('input[name="username"]').value = data.username;
-        document.querySelector('input[name="password"]').value = data.password;
-        document.forms[0].submit();
-    }, data);
-
-    await page.waitFor(5000);
-    await page.goto(url.logBookPage, {waitUntil: 'networkidle2'});
-
-    //fill logbook
-    await page.evaluate(function(data){
-        document.querySelector('input[name="clock-in"]').value = data.clock.in;
-        document.querySelector('input[name="clock-out"]').value = data.clock.out;
-        document.querySelector('input[name="activity"]').value = data.activity;
-        document.querySelector('textarea').value = data.description;
-        document.forms[4].submit();
-    }, data);
-
-    await page.waitFor(5000);
-    await browser.close();
-}*/
-
+// event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
     return Promise.resolve(null);
   }
 
+  // create a echoing text message
   const echo = { type: 'text', text: event.message.text };
-  //await fill(message.text);
 
-  return client.replyMessage(event.replyToken, echo.text);
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
 }
 
+// listen on port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`listening on ${port}`);
