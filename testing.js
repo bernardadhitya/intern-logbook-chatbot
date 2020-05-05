6a1ae42878a8c07533ae0702d8c4e138ea68dedf
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-05-05 18:10:40 +0700

    [Bernard] Mark class Show as in development

--- testing.js
+++ testing.js
@@ -89,14 +89,15 @@ function getMonth(){
 console.log(getMonth());
 */
 
+
 const Show = require('./commands/show.js');
 const show = new Show('03/03/2020');
+
 console.log(show.run());
+
 
 // const msg = 'Fri, 03/05/1999';
 // console.log(msg.split(', ')[1]);
 
 //console.log(show.getMonth());
-
-
 
