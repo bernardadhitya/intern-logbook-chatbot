890fc5c6ca5777a8d66ea450aa43cbbb699fa5f1
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-05-05 18:49:08 +0700

    [Bernard] Integrate features to automate process log collection

---
+++ utils/collectAll.js
@@ -0,0 +1,89 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+let completeLog = require('../logs/data.json');
+require ('dotenv').config();
+
+(async () => {
+    const data = {
+        username: process.env.MY_USERNAME,
+        password: process.env.MY_PASSWORD,
+        clock: {
+            in: "-",
+            out: "-"
+        },
+        activity: "Day Off",
+        description: "Day Off"
+    }
+    
+    let url = {
+        loginPage: 'https://industry.socs.binus.ac.id/learning-plan/auth/login',
+        logBookPage: 'https://industry.socs.binus.ac.id/learning-plan/student/log-book'
+    }
+
+    let browser = await puppeteer.launch({
+        headless: true
+    });
+    let page = await browser.newPage();
+
+    await page.goto(url.loginPage, {waitUntil: 'networkidle2'});
+
+    //fill login
+    await page.evaluate(function(data){
+        document.querySelector('input[name="username"]').value = data.username;
+        document.querySelector('input[name="password"]').value = data.password;
+        document.forms[0].submit();
+    }, data);
+
+    await page.waitFor(5000);
+    await page.goto(url.logBookPage, {waitUntil: 'networkidle2'});
+
+    //collect all data
+    let allLogs = await page.evaluate(function(){
+        const today = new Date();
+        const currDate =
+                    (today.getDate() < 10 ? '0' + today.getDate() : today.getDate()) + '/' + 
+                    (today.getMonth() < 10 ? '0' + (today.getMonth()+1) : today.getMonth()+1) + '/' + 
+                    today.getFullYear();
+
+        let allLogs = [];
+        const logs = document.querySelectorAll('table tbody tr');
+            
+        for(let j=0; j<logs.length; j++){
+            let log = {
+                date: '',
+                clock: {
+                    in: 'n/a',
+                    out: 'n/a'
+                },
+                activity: 'n/a',
+                description: 'n/a'
+            }
+
+            let date = logs[j].querySelector('td.date').innerText;
+            date = date.split(', ')[1];
+            log.date = date;
+
+            if(date === currDate){
+                break;
+            }
+
+            log.clock.in = logs[j].querySelector('td.clock-in').innerText;
+            log.clock.out = logs[j].querySelector('td.clock-out').innerText;
+            log.activity = logs[j].querySelector('td.activity').innerText;
+            log.description = logs[j].querySelector('td.description').innerText;
+            
+            allLogs.push(log);
+        }
+        
+        return allLogs;
+    });
+
+    for(let i=0; i<allLogs.length; i++){
+        completeLog.data.push(allLogs[i]);
+    }
+    
+    await page.waitFor(5000);
+    await browser.close();
+
+    fs.writeFile('../logs/data.json', JSON.stringify(completeLog, null, 4), err => err ? console.log(err) : null);
+})();
---
+++ utils/fillWeekend.js
@@ -0,0 +1,51 @@
+const puppeteer = require('puppeteer');
+require ('dotenv').config();
+
+(async () => {
+    const data = {
+        username: process.env.MY_USERNAME,
+        password: process.env.MY_PASSWORD,
+        clock: {
+            in: "-",
+            out: "-"
+        },
+        activity: "Day Off",
+        description: "Day Off"
+    }
+    
+    let url = {
+        loginPage: 'https://industry.socs.binus.ac.id/learning-plan/auth/login',
+        logBookPage: 'https://industry.socs.binus.ac.id/learning-plan/student/log-book'
+    }
+
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+
+    await page.goto(url.loginPage, {waitUntil: 'networkidle2'});
+
+    //fill login
+    await page.evaluate(function(data){
+        document.querySelector('input[name="username"]').value = data.username;
+        document.querySelector('input[name="password"]').value = data.password;
+        document.forms[0].submit();
+    }, data);
+
+    await page.waitFor(5000);
+    await page.goto(url.logBookPage, {waitUntil: 'networkidle2'});
+
+    //fill logbook
+    await page.evaluate(function(data){
+        //check if its day off
+        let currDate = new Date(Date.now());
+        if(currDate.getDay() == 0 || currDate.getDay() == 6){
+            document.querySelector('input[name="clock-in"]').value = data.clock.in;
+            document.querySelector('input[name="clock-out"]').value = data.clock.out;
+            document.querySelector('input[name="activity"]').value = data.activity;
+            document.querySelector('textarea').value = data.description;
+            document.forms[4].submit();
+        }
+    }, data);
+
+    await page.waitFor(5000);
+    await browser.close();
+})();
